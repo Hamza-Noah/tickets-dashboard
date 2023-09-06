49473946dc324a37d994436bbe31614011888108
Hamza-Noah <hamza.nouh97@gmail.com>
2023-09-06 04:20:31 +0300

    added the last column to the table

--- src/app/tickets-sold/tickets-sold.component.ts
+++ src/app/tickets-sold/tickets-sold.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 interface ticket {
-
   ticketCount: number;
   user: object;
   ticketNumber: string;
@@ -12,72 +11,58 @@ interface ticket {
   ticket: object;
 }
 
-
-
 @Component({
   selector: 'app-tickets-sold',
   templateUrl: './tickets-sold.component.html',
-  styleUrls: ['./tickets-sold.component.scss']
+  styleUrls: ['./tickets-sold.component.scss'],
 })
 export class TicketsSoldComponent {
-  imgSrc: string = "../../assets/images/";
+  imgSrc: string = '../../assets/images/';
   // @ts-ignore
   selectedRows: number;
+  totalRecords: number = 100; // Replace with the actual total record count
+  first: number = 0;
 
   tickets: ticket[] = [
     {
       ticketCount: 1,
-      user: {name: 'أحمد محمود', phone: "0123456789" },
-      ticketNumber: 'C-101',  // Updated
+      user: { name: 'أحمد محمود', phone: '0123456789' },
+      ticketNumber: 'C-101', // Updated
       ticketPrice: 40,
       cinema: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة ...',
       numberOfPeople: 1,
       numberOfPurchases: 'مرة واحدة',
-      ticket: { type: 'Regular' },
+      ticket: { ticketNumber: 'تذكرة رقم pdf.45', file: 'PDF', size: '9mb' },
     },
     {
       ticketCount: 2,
-      user: { img: "user-1.jpg", name: 'محمد الغريب', phone: "0123456789" },
-      ticketNumber: 'C-102',  // Updated
+      user: { img: 'user-1.jpg', name: 'محمد الغريب', phone: '0123456789' },
+      ticketNumber: 'C-102', // Updated
       ticketPrice: 30,
       cinema: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة ...',
       numberOfPeople: 2,
       numberOfPurchases: 'مرة واحدة',
-      ticket: { type: 'VIP' },
+      ticket: { ticketNumber: 'تذكرة رقم pdf.45', file: 'PDF', size: '9mb' },
     },
     {
       ticketCount: 3,
-      user: { img: "user-2.jpg", name: 'محمد الغريب', phone: "0123456789" },
-      ticketNumber: 'C-103',  // Updated
+      user: { img: 'user-2.jpg', name: 'محمد الغريب', phone: '0123456789' },
+      ticketNumber: 'C-103', // Updated
       ticketPrice: 45,
       cinema: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة ...',
       numberOfPeople: 1,
       numberOfPurchases: 'مرة واحدة',
-      ticket: { type: 'Matinee' },
+      ticket: { ticketNumber: 'تذكرة رقم pdf.45', file: 'PDF', size: '9mb' },
     },
     {
       ticketCount: 4,
-      user: { name: 'محمد الغريب', phone: "0123456789" },
-      ticketNumber: 'C-104',  // Updated
+      user: { name: 'محمد الغريب', phone: '0123456789' },
+      ticketNumber: 'C-104', // Updated
       ticketPrice: 54,
       cinema: 'سينما مصر , مول العرب , برج الأطنان , سرايا القبة ...',
       numberOfPeople: 1,
       numberOfPurchases: 'مرة واحدة',
-      ticket: { type: 'Matinee' },
+      ticket: { ticketNumber: 'تذكرة رقم pdf.45', file: 'PDF', size: '9mb' },
     },
   ];
-
-
-  totalRecords: number = 100; // Replace with the actual total record count
-  first: number = 0;
-
-  onPageChange(event: any) {
-    this.first = event.first;
-    // Load data based on the new page
-  }
-
 }
-
-
-
-
